200474c0aaa7df8dc88a87ef2b5915daf59655ec
Dima Timoshenko <tzd0409@gmail.com>
2017-09-02 01:07:36 +0300

    PostCreateView

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    url(r'^$', PostListView.as_view(),  name='index'),
+    url(r'^$', PostListView.as_view(),  name='list_view'),
     url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail_view'),
+    url(r'^create/$', PostCreateView.as_view(), name='create_view'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
@@ -13,5 +16,27 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post/detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    success_url = reverse_lazy('post:list_view')
+    template_name = 'post/create.html'
+    fields = ['title', 'content']
+
+    def get_form(self, form_class=None):
+        form = super(PostCreateView, self).get_form(form_class)
+        for field in form.fields:
+            form.fields[field].widget.attrs.update({'class': 'form-control'})
+        return form
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        message = 'Post successfully created.'
+        messages.success(self.request, message)
+        return super(PostCreateView, self).form_valid(form)
+
 
 
